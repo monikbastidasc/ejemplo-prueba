5ef2c3e0f4ac570312849b9c0101962702eabbe7
monikbastidasc <monikbastidasc@gmail.com>
2022-06-16 14:59:26 -0500

    Creacion de clases

---
+++ src/main/java/com/pichincha/prueba/demo/entity/DetailOrder.java
@@ -0,0 +1,36 @@
+package com.pichincha.prueba.demo.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class DetailOrder {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "order_detail_id")
+	private Long id;
+	
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+	@JoinColumn(name = "order_id", nullable = false)
+	private ProductOrder orderOwner;
+	
+	private Integer totalOdered;
+	private Double priceUnit;
+	private Double priceTotal;
+
+}
